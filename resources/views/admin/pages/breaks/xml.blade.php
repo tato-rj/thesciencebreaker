708d93f0b907a6fd7a0596f1234d0c3c64497623
Arthur Villar <arthur@iMac.local>
2022-09-07 18:13:32 -0400

    Add xml funcionality

--- resources/views/admin/pages/breaks/xml.blade.php
+++ resources/views/admin/pages/breaks/xml.blade.php
@@ -4,17 +4,17 @@
   
     <div class="container-fluid mb-4">
       
-{{--       @component('admin/snippets/page_title')
+      @component('admin/snippets/page_title')
           Breaks
         @slot('comment')
           Use the form below to add a break by <u>uploading</u> an xml file
         @endslot
-      @endcomponent --}}
+      @endcomponent
 
       <div class="row mt-4">
         <div class="col-lg-8 col-md-10 col-sm-12 mx-auto border p-3">
           <form id="xml-form" action="{{route('xml')}}" method="post" enctype="multipart/form-data">
-            @csrf
+            {{csrf_field()}}
             <div class="form-group">
               <input type="file" id="xml-input" name="xml" class="form-control-file" required>
             </div>
