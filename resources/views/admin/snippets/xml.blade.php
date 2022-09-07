605860c2a97144a8c516d1255fe5a31a80782fee
Arthur Villar <arthur@iMac.local>
2022-09-07 14:10:04 -0400

    Add xml funcionality

--- resources/views/admin/snippets/xml.blade.php
+++ resources/views/admin/snippets/xml.blade.php
@@ -1,10 +1,12 @@
 <div class="container mt-4 border p-3">
-  <form id="xml-form" action="{{route('xml')}}" method="post" enctype="multipart/form-data">
-    @csrf
-    <div class="form-group">
-      <input type="file" id="xml" name="xml" class="form-control">
-    </div>
+  <div class="col-lg-8 col-md-10 col-sm-12 mx-auto">
+    <form id="xml-form" action="{{route('xml')}}" method="post" enctype="multipart/form-data">
+      @csrf
+      <div class="form-group">
+        <input type="file" id="xml" name="xml" class="form-control">
+      </div>
 
-    <button type="submit" class="btn btn-theme-orange">Upload XML</button>
-  </form>
+      <button type="submit" class="btn btn-dark">Upload XML</button>
+    </form>
+  </div>
 </div>
