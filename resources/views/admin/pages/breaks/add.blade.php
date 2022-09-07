eeeb9fdb693d5680a4adb69a9e2cc38ac079e64b
Arthur Villar <arthur@iMac.local>
2022-09-07 13:13:41 -0400

    Add xml funcionality

--- resources/views/admin/pages/breaks/add.blade.php
+++ resources/views/admin/pages/breaks/add.blade.php
@@ -236,7 +236,7 @@
       </div>
     </div>
 
-<form id="xml-form" action="{{route('xml')}}" method="post" enctype="multipart/form-data">
+<form id="xml-form" action="{{route('xml')}}" method="post" enctype="multipart/form-data" style="opacity: 0;">
   @csrf
   <input type="file" id="xml" name="xml">
   <button type="submit">Upload XML</button>
