298d2e448d3a253a4c4de5323546b8aa8aedb587
Arthur Villar <arthur@iMac.local>
2023-01-05 09:30:07 -0500

    Quick update

--- app/Manager/Traits/ArticleScopeQueries.php
+++ app/Manager/Traits/ArticleScopeQueries.php
@@ -31,7 +31,7 @@ trait ArticleScopeQueries
 
     public function scopeByTitle($query, $title)
     {
-        return $query->whereRaw('lower(language) like (?)', strtolower($title));
+        return $query->whereRaw('lower(title) like (?)', strtolower($title));
     }
 
     public function scopePicks($query)
