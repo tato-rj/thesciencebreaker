590639889f057d550a2243fd43265562d422ad46
Arthur Villar <arthur@iMac.local>
2023-01-05 10:19:53 -0500

    Quick update

--- resources/views/admin/pages/breaks/xml.blade.php
+++ resources/views/admin/pages/breaks/xml.blade.php
@@ -35,7 +35,7 @@
 
             {{-- Reading time --}}
             <div class="form-group">
-              <div class="input-group col-3 pl-0">
+              <div class="input-group">
                 <div class="input-group-addon"><i class="fa fa-hourglass-half" aria-hidden="true"></i></div>
                 <input required type="text" value="{{ old('reading_time') }}" name="reading_time" size="4" class="form-control" id="reading_time" placeholder="Reading time">
               </div>
@@ -46,7 +46,7 @@
               @endslot
               @endcomponent
             </div>   
-            
+
             {{-- Caption --}}
             <div>
                 <div class="form-group">
