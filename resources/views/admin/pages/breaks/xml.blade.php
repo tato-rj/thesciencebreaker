d2b30678bf45441a96a1375e820c0656cd6136f1
Arthur Villar <arthur@iMac.local>
2023-01-05 10:16:07 -0500

    Quick update

--- resources/views/admin/pages/breaks/xml.blade.php
+++ resources/views/admin/pages/breaks/xml.blade.php
@@ -11,7 +11,8 @@
         @endslot
       @endcomponent
 
-      <div class="mt-4">
+      <div class="row mt-4">
+        <div class="col-lg-8 col-md-10 col-sm-12 mx-auto">
           <form id="xml-form" action="{{route('xml')}}" method="post" enctype="multipart/form-data">
             {{csrf_field()}}
             <div class="form-group">
@@ -66,6 +67,7 @@
 
             <button type="submit" class="btn btn-dark">Upload XML</button>
           </form>
+        </div>
       </div>
     </div>
 @endsection
