3f8d8641494fa2d9bf09c9f7ded176f9962e4e5c
Arthur Villar <arthur@iMac.local>
2023-01-06 13:18:14 -0500

    Quick update

--- resources/views/components/alerts/error.blade.php
+++ resources/views/components/alerts/error.blade.php
@@ -5,9 +5,9 @@
 	@slot('message')
 		<i class="fa fa-exclamation-triangle" aria-hidden="true"></i> <strong>{{ $flash or 'Ops, something went wrong...'}}</strong>
 		@if($errors->any())
-		<ul class="m-0 p-1 pl-3">
+		<ul class="m-0 p-1 pl-3" style="list-style: none;">
 			@foreach ($errors->all() as $error)
-				<li><small class="text-danger">{{ $error }}</small></li>
+				<li class="text-danger"><small>{{ $error }}</small></li>
 			@endforeach
 		</ul>
 		@endif
