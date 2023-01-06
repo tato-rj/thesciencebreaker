f94c67775ce5463c66e7bb73f394d7e5e5c6ecec
Arthur Villar <arthur@iMac.local>
2023-01-06 11:53:47 -0500

    Quick update

--- app/Xml/Validator.php
+++ app/Xml/Validator.php
@@ -11,26 +11,20 @@ class Validator
 
 	public function validate()
 	{
-		if (! $this->request['title'])
-			dd('The title is missing');
+		$data = [
+			'title' => $this->request['title'] ?? null,
+			'description' => $this->request['abstract'] ?? null,
+			'image' => $this->request['coverjs']['cover']['cover_image'] ?? null,
+			'reading_time' => $this->request['subjects']['subject'] ?? null,
+			'original_article' => $this->request['citations']['citation'] ?? null,
+			'category' => $this->request['@attributes']['section_ref'] ?? null,
+			'doi' => $this->request['id'] ?? null,
+		];
 
-		if (! $this->request['abstract'])
-			dd('The description is missing');
-
-		if (! $this->request['coverjs']['cover']['cover_image'])
-			dd('The image is missing');
-
-		if (! $this->request['subjects']['subject'])
-			dd('The reading time is missing');
-
-		if (! $this->request['citations']['citation'])
-			dd('The original article is missing');
-
-		if (! $this->request['@attributes']['section_ref'])
-			dd('The category is missing');
-
-		if (! $this->request['id'])
-			dd('The doi is missing');
+		foreach ($data as $field => $value) {
+			if (! $value)
+				dd('The ' . $field . ' is missing');
+		}
 
 		return $this->request;
 	}
