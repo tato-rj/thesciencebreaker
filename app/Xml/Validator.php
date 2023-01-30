55ea05c6aea4245d0b9b0f74a89433e3dfdfbba8
Arthur Villar <arthur@iMac.local>
2023-01-30 10:40:50 -0500

    Quick update

--- app/Xml/Validator.php
+++ app/Xml/Validator.php
@@ -22,6 +22,7 @@ class Validator
 		$data = [];
 
 		foreach ($breakers as $breaker) {
+			dd($breaker['givenname']);
 			$info = [
 				'first_name' => $breaker['givenname'] ?? null,
 				'last_name' => $breaker['familyname'] ?? null,
